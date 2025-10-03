68c7481ccfb845375eda9aabfa9929ba5dca65c2
MingMinNa <n139726845@gmail.com>
2025-10-03 16:11:06 +0800

    Update: new args & log format & oversized image condition

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-## Image Upscaling Tool
+# Image Upscaling Tool
 
-### 專案介紹
-本專案基於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 所開發，旨在對圖片進行畫質增強處理。執行 `main.py` 後，系統將自動讀取 `image` 資料夾中的所有圖片，進行畫質提升，並將處理後的結果輸出至 `output` 資料夾中。
+## 專案介紹
+本專案基於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 所開發，旨在對圖片進行畫質增強處理。  
+執行 `main.py` 後，系統將對輸入資料夾中的所有圖片，進行畫質增強，並將處理後的結果輸出至輸出資料夾中。
 
----
-
-### 專案用法
+## 專案用法
 
 #### 1. 創建與開啟虛擬環境
 使用下列指令建立虛擬環境，並啟用它（請將 `<venv_name>` 替換為自訂的環境名稱）
@@ -21,15 +20,26 @@ pip install -r requirements.txt
 
 #### 3. 下載模型權重
 請將下載後的模型權重檔案放入專案根目錄下的 `weights/` 資料夾中。
-#### 4. 放置輸入圖片
-將欲提升解析度的圖片檔案放入 `image/` 資料夾中。
-#### 5. 執行主程式
-透過以下指令執行主程式 `main.py`，可使用 `--factor` 參數指定所需的模型倍率。
-```bash
-python main.py --factor <倍率>
-```
----
 
-### 注意事項
-> 由於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 的原始碼相對較舊，為使其能順利執行並符合現今的執行環境，本專案對其程式碼進行了少許的調整與修改。
-為方便使用，已將修改後的原始碼直接包含於本專案中。但需特別聲明，該程式碼之著作權仍歸屬於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 專案原作者所有。
+#### 4. 放置輸入圖片
+將欲提升解析度的圖片檔案放入 `image/` 資料夾中，或另外指定輸入資料夾。
+
+#### 5. 執行主程式
+執行 `main.py`，並可透過參數設定模型倍率、輸入與輸出路徑等選項。
+
+```bash
+參數介紹
+    --factor        可選，放大倍率
+    --input         可選，輸入資料夾路徑
+    --output        可選，輸出資料夾路徑
+    --overwrite     可選，是否覆寫檔案
+
+參數數值範圍與預設值
+    --factor        [2, 4, 8]             預設為 2
+    --input         路徑字串               預設為 "./image"
+    --output        路徑字串               預設為 "./output"
+    --overwrite     旗標（無需數值）        預設為 False
+```
+
+## 注意事項
+> 由於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 的原始碼相對較舊，為使其能順利執行並符合現今的執行環境，本專案對其程式碼進行了少許的調整與修改，並且已將修改後的原始碼直接包含於本專案中。但需特別聲明，該程式碼之著作權仍歸屬於 [Real-ESRGAN](https://github.com/ai-forever/Real-ESRGAN) 專案原作者所有。
--- main.py
+++ main.py
@@ -2,100 +2,102 @@ import os
 import argparse
 import shutil
 import torch
+
 from tqdm import tqdm
 from PIL import Image
 from RealESRGAN import RealESRGAN
 
 PROJECT_FOLDER = os.path.dirname(__file__)
+MAX_BOUND = 6000
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+factor = None
 model = None
 model_path = None
-upscaling_bound = (None, None)
 
 
-def search_folders(folder_path):
-    '''
-    Given a folder path, search all the sub folder paths(include itself). 
-    Return them as a list of string
-    '''
+def check_folder(folder_path: str) -> None: 
+    
     if os.path.exists(folder_path) == False:
         raise FileExistsError(f"{folder_path} doesn't exist.")
     elif os.path.isfile(folder_path):
         raise ValueError(f"{folder_path} is a file, not folder.")
-    
-    sub_folder_paths = [folder_path]
-    for name in os.listdir(path = folder_path):
-        path = os.path.join(folder_path, name)
-        if os.path.isdir(path):
-            sub_folder_paths += search_folders(path)
+    return
 
-    return sub_folder_paths
-
-def upscale_image(src_img_path, dest_img_path):
-    global upscaling_bound
+def upscale_image(src_img_path: str, dest_img_path: str) -> bool:
 
     src_img = Image.open(src_img_path).convert('RGB')
-    if src_img.size[0] > upscaling_bound[0] and src_img.size[1] > upscaling_bound[1]:
+    if (src_img.size[0] * factor > MAX_BOUND or 
+        src_img.size[1] * factor > MAX_BOUND):
+
         shutil.copy(src_img_path, dest_img_path)
         return False
-    else:
-        dest_img = model.predict(src_img)
-        dest_img.save(dest_img_path)
+    
+    dest_img = model.predict(src_img)
+    dest_img.save(dest_img_path)
     return True
 
-def check_file_format(file_path):
-    if file_path.endswith('.png') or \
-       file_path.endswith('.jpg') or \
-       file_path.endswith('.jpeg') or \
-       file_path.endswith('.webp'):
-        return True
+def check_file_format(file_path: str) -> bool:
+
+    valid_formats = ['.png', '.jpg', '.jpeg', '.webp']
+
+    for ext in valid_formats:
+        if file_path.lower().endswith(ext):
+            return True
     return False 
 
-def init_parser():
+def init_parser() -> argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser(description = 'This is an image-upscaling tool')
     parser.add_argument('--factor', type = int, choices = [2, 4, 8], default = 2, help = 'set upscaling factor(x2, x4, x8)')
+    parser.add_argument('--input' , type = str, default = os.path.join(PROJECT_FOLDER, 'image') , help = 'set input folder path')
+    parser.add_argument('--output', type = str, default = os.path.join(PROJECT_FOLDER, 'output'), help = 'set output folder path')
+    parser.add_argument('--overwrite', action = "store_true", help = 'set whether to overwrite the existing files')
     return parser
 
-def select_model(scaling_factor):
-    global model, model_path, upscaling_bound
+def select_model(scaling_factor: int) -> None:
+    global factor, model, model_path
 
     if scaling_factor not in (2, 4, 8):
         raise ValueError('scaling_factor is out of bound')
 
-    model = RealESRGAN(device, scale = scaling_factor)
-    model_path = os.path.join('weights', f'RealESRGAN_x{scaling_factor:1d}.pth')
+    factor = scaling_factor
+    model = RealESRGAN(device, scale = factor)
+    model_path = os.path.join(PROJECT_FOLDER, 'weights', f'RealESRGAN_x{factor:1d}.pth')
     model.load_weights(model_path, download = True)
-    upscaling_bound = (3000 / scaling_factor, 3000 / scaling_factor)
-    
     return
 
-def main():
+def main() -> None:
+
     parser = init_parser()
     args = parser.parse_args()
-    select_model(scaling_factor = int(args.factor))
-
-    raw_folders_abspath = search_folders(os.path.join(PROJECT_FOLDER, 'image'))
     
-    replace_func = lambda path: path.replace(os.path.join(PROJECT_FOLDER, 'image'), os.path.join(PROJECT_FOLDER, 'output'))
+    check_folder(os.path.abspath(args.input))
+    select_model(scaling_factor = int(args.factor))
+    
+    replace_func = lambda path: path.replace(os.path.abspath(args.input), os.path.abspath(args.output))
 
-    for raw_folder_path in tqdm(raw_folders_abspath):
-        tqdm.write(f"Currently Processed Folder: {raw_folder_path}")
-        output_folder_path = replace_func(raw_folder_path)
+    for root, _, files in tqdm(os.walk(os.path.abspath(args.input))):
 
-        os.makedirs(output_folder_path, exist_ok = True)
-        for name in tqdm(os.listdir(raw_folder_path)):
-            raw_file_path = os.path.join(raw_folder_path, name)
-            output_file_path = replace_func(raw_file_path)
-            if not os.path.isfile(raw_file_path) or \
-               os.path.exists(output_file_path): continue
+        tqdm.write(f"Currently Processed Folder: {root}")
+        
+        out_folder_path = replace_func(root)
+        os.makedirs(out_folder_path, exist_ok = True)
 
-            if check_file_format(raw_file_path):
-                upscaling = upscale_image(src_img_path = raw_file_path, dest_img_path = output_file_path)
-                if upscaling:   tqdm.write(f"Upscaling Image: {raw_file_path}")
-            else:
-                tqdm.write(f"file path:\"{raw_file_path}\" doesn't be converted successfully!")
+        for name in files:
+
+            in_file_path = os.path.join(root, name)
+            out_file_path = replace_func(in_file_path)
+
+            if not args.overwrite and os.path.exists(out_file_path): 
+                continue
+
+            if check_file_format(in_file_path):
+                upscaling = upscale_image(in_file_path, out_file_path)
+                if upscaling:   tqdm.write(f"\033[1;32m[INFO]\033[0m    Upscaled: {in_file_path}")
+                else:           tqdm.write(f"\033[1;33m[WARNING]\033[0m Image too large, copied: {in_file_path}") 
+            else:               tqdm.write(f"\033[1;31m[ERROR]\033[0m   Unsupported format: {in_file_path}")
+    return
 
 if __name__ == '__main__':
     main()
